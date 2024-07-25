316d6975e2d646813fd7cd372b38c36c9a02089e
Tom Schwindl <schwindl@posteo.de>
2024-07-26 00:00:32 +0200

    ui: first version of simple login screen

--- source/ui-login.py
+++ source/ui-login.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 import curses
+import curses.textpad
 
 screen = curses.initscr()
 
@@ -17,15 +17,31 @@ def resetTerm():
     screen.keypad(False)
     curses.endwin()
 
-def drawUsername():
+def drawText():
     screen.addstr(6, 10, "Username:")
-
-def drawPassword():
     screen.addstr(11, 10, "Password:")
+    screen.refresh()
+
+def drawInput():
+    curses.flash()
+
+    winUsername = curses.newwin(1, 30, 6, 22)
+    winPassword = curses.newwin(1, 30, 11, 22)
+    username = curses.textpad.Textbox(winUsername, insert_mode=True)
+    password = curses.textpad.Textbox(winPassword, insert_mode=True)
+    inputUsername = username.edit()
+    inputPassword = password.edit()
+    winUsername.clear()
+    winPassword.clear()
+    winUsername.refresh()
+    winPassword.refresh()
+    winUsername.getch()
+    winPassword.getch()
 
 
 # TODO: move to main.py (but for testing let it be here)
 initTerm()
-drawUsername()
-drawPassword()
+drawText()
+drawInput()
 screen.refresh()
+#resetTerm()
