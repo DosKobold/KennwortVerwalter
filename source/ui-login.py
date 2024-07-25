6fb155f36b2129129d3b32c23d331704dadfe779
Tom Schwindl <schwindl@posteo.de>
2024-07-25 23:46:13 +0200

    ui: add a bit of text

--- source/ui-login.py
+++ source/ui-login.py
@@ -4,18 +4,28 @@ import curses
 
 screen = curses.initscr()
 
+# Initialize terminal screen.
 def initTerm():
-    # Initialize terminal screen.
     curses.noecho()
     curses.cbreak()
     screen.keypad(True)
     
-    # Reset terminal screen (so it won't be messed up afterwards).
+# Reset terminal screen (so it won't be messed up afterwards).
+def resetTerm():
     curses.echo()
     curses.nocbreak()
-    curses.keypad(False)
+    screen.keypad(False)
     curses.endwin()
+
+def drawUsername():
+    screen.addstr(6, 10, "Username:")
+
+def drawPassword():
+    screen.addstr(11, 10, "Password:")
 
 
 # TODO: move to main.py (but for testing let it be here)
 initTerm()
+drawUsername()
+drawPassword()
+screen.refresh()
