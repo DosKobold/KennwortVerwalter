0a43041b6c73480e6399366b26f99a2bf9d83b4e
DosKobold <hanspeter56@t-online.de>
2024-07-27 12:54:33 +0200

    Changed pylintrc, finished first version of Cryptor

--- source/cryptor.py
+++ source/cryptor.py
@@ -1,171 +1,185 @@
 #!/bin/python3
+"""
+File: Cryptor.py
+Desc: Implements a simple interface for the Datahandler. En- and decrypts text with a masterkey.
+      Hashes a password to a masterkey. Checks if a password is the mastekey. Generates and checks
+      passwords. 
+"""
 
-from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import os
 import hashlib
 import secrets
-import os
 import base64
 import string
 import requests
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 class Cryptor:
+    """Class for crypting and hashing"""
 
-	__fernet = None
+    __fernet = None
 
-	def __setMasterKey(self, key: str):
-		key = key.encode("utf-8")
-		salt = os.urandom(16)
-		kdf = PBKDF2HMAC(
-			algorithm=hashes.SHA256(),
-			length=32,
-			salt=salt,
-			iterations=480000,
-		)
-		masterKey = base64.urlsafe_b64encode(kdf.derive(key))
-		self.__fernet = Fernet(masterKey)
+    def __setMasterKey(self, key: str) -> None:
+        salt = os.urandom(16)
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=480000,
+        )
+        masterKey = base64.urlsafe_b64encode(kdf.derive(key.encode("utf-8")))
+        self.__fernet = Fernet(masterKey)
 
-	def __wrongUsage(self):
-		print("[Cryptor] ERROR: Wrong usage of Cryptor! Wrong order of method calls! No master key set!")
-		exit()
+    def __wrongUsage(self) -> None:
+        print("[Cryptor] ERROR: Wrong usage of Cryptor! Wrong order of method calls! No master key set!")
+        exit()
 
-	def hashPassword(self, text: str, externalCall: bool) -> str:
-		sha3_512 = hashlib.sha3_512()
-		sha3_512.update(text.encode("utf-8"))
-		if(externalCall):
-			self.__setMasterKey(text)
-		return sha3_512.hexdigest()
-		
-	def isCorrectPassword(self, text: str, sha3_512 :str) -> bool:
-		isCorrect = False
-		if(self.hashPassword(text, False) == sha3_512):
-			isCorrect = True
-			self.__setMasterKey(text)
-		return isCorrect
+    def hashKey(self, key: str, externalCall: bool) -> str:
+        hashedKey = hashlib.sha3_512(key.encode("utf-8"))
+        if externalCall:
+            self.__setMasterKey(key)
+        return hashedKey.hexdigest()
 
-	def encryptText(self, text: str) -> str:
-		if(self.__fernet is None):
-			self.__wrongUsage()
-		token = self.__fernet.encrypt(text.encode("utf-8"))
-		return token.decode("utf-8")
+    def isCorrectKey(self, key: str, hashedKey :str) -> bool:
+        """Checks if a given password is the same as the hashed masterkey"""
+        isCorrect = False
+        if self.hashKey(key, False) == hashedKey:
+            isCorrect = True
+            self.__setMasterKey(key)
+        return isCorrect
 
-	def decryptText(self, text: str) -> str:
-		if(self.__fernet is None):
-			self.__wrongUsage()
-		token = self.__fernet.decrypt(text.encode("utf-8"))
-		return token.decode("utf-8")
+    def encryptText(self, text: str) -> str:
+        """Encrypts a given text with the master key"""
+        if self.__fernet is None:
+            self.__wrongUsage()
+	#None has no attribute "encrypt" -> None is checked above
+        token = self.__fernet.encrypt(text.encode("utf-8")) #type: ignore
+        return token.decode("utf-8")
+
+    def decryptText(self, text: str) -> str:
+        """Decrypts a given text with the master key"""
+        if self.__fernet is None:
+            self.__wrongUsage()
+	#None has no attribute "decrypt" -> None is checked above
+        token = self.__fernet.decrypt(text.encode("utf-8")) #type: ignore
+        return token.decode("utf-8")
 
 
-	def genPassword(self, length: int, digits: bool, others: bool, upper: bool, lower: bool, forbidden: str) -> str:
-		chars    = 0
-		alphabet = str(None)
+    def genPassword(self, length: int, digits: bool, others: bool, upper: bool, lower: bool, forbidden: str) -> str:
+        """Generates a password with several options"""
+        chars    = 0
+        alphabet = str(None)
 
-		if(digits):
-			chars    += 1
-			alphabet += string.digits
-		if(others):
-			chars    += 1
-			alphabet += string.punctuation
-		if(upper) :
-			chars    += 1
-			alphabet += string.ascii_uppercase
-		if(lower) :
-			chars    += 1
-			alphabet += string.ascii_lowercase
+        if digits:
+            chars    += 1
+            alphabet += string.digits
+        if others:
+            chars    += 1
+            alphabet += string.punctuation
+        if upper :
+            chars    += 1
+            alphabet += string.ascii_uppercase
+        if lower :
+            chars    += 1
+            alphabet += string.ascii_lowercase
 
-		if((alphabet is str(None)) or (chars > length)):
-			 return str("")
+        if (alphabet is str(None)) or (chars > length):
+            return str("")
 
-		while True:
-			password = ''.join(secrets.choice(alphabet) for i in range(length))
-			if(digits): 
-				if(not any(char.isdigit() for char in password)): continue
-			if(others): 
-				if(not any(char in string.punctuation for char in password)): continue
-			if(upper) : 
-				if(not any(char.isupper() for char in password)): continue
-			if(lower): 
-				if(not any(char.islower() for char in password)): continue
-			if(forbidden != ""):
-				if(forbidden in password): continue
-			break
+        while True:
+            password = ''.join(secrets.choice(alphabet) for i in range(length))
+            if digits:
+                if not any(char.isdigit() for char in password):
+                    continue
+            if others:
+                if not any(char in string.punctuation for char in password):
+                    continue
+            if upper :
+                if not any(char.isupper() for char in password):
+                    continue
+            if lower:
+                if not any(char.islower() for char in password):
+                    continue
+            if forbidden != "":
+                if forbidden in password:
+                    continue
+            break
 
-		return str(password)
+        return str(password)
 
-	def isSafe(self, text: str) -> tuple[bool, str]:
-		isSecure  = True
-		message = ""
+    def isSafe(self, text: str) -> tuple[bool, str]:
+        """Checks if a given password is safe. Provides a additional description string"""
+        isSecure  = True
+        message = ""
 
-		if(not any(char.isdigit() for char in text)):
-			isSecure = False
-			message += "Digit missing|"
-		if(not any(char in string.punctuation for char in text)):
-			isSecure = False
-			message += "Punctuation missing|"
-		if(not any(char.isupper() for char in text)):
-			isSecure = False
-			message += "Uppercase letter missing|"
-		if(not any(char.islower() for char in text)):
-			isSecure = False
-			message += "Lowercase letter missing|"
-		if(len(text) < 8):
-			isSecure = False
-			message += "Password under 8 characters long|"
+        if not any(char.isdigit() for char in text):
+            isSecure = False
+            message += "Digit missing|"
+        if not any(char in string.punctuation for char in text):
+            isSecure = False
+            message += "Punctuation missing|"
+        if not any(char.isupper() for char in text):
+            isSecure = False
+            message += "Uppercase letter missing|"
+        if not any(char.islower() for char in text):
+            isSecure = False
+            message += "Lowercase letter missing|"
+        if len(text) < 8:
+            isSecure = False
+            message += "Password under 8 characters long|"
 
-		#Search password in the haveibeenpwned database
-		#TODO --- Not finished, complete mess! ---
-		sha1 = hashlib.sha1()
-		sha1.update(text.encode("utf-8"))
-		sha1 = sha1.hexdigest()
-		sha1 = str(sha1)
-		response = requests.get(f"https://api.pwnedpasswords.com/range/{sha1[:5]}")
-		if(response.status_code == 200):
-			if(sha1[:5] in response.text):
-				isSecure = False
-				message += "Password is well known"
-		print(response.status_code)
-		print(sha1)
-		print(response.text)
-		#-----------------------------------------
-		return (isSecure, message)
+        #Search password in the haveibeenpwned database
+        url = "https://api.pwnedpasswords.com/range/"
+        sha1 = str(hashlib.sha1(text.encode("utf-8")).hexdigest()).upper()
+        response = requests.get(url + sha1[:5])
+        if response.status_code == 200:
+            for line in response.text.splitlines():
+                if sha1[5:] in line:
+                    isSecure = False
+                    message += "Password is well known|"
+        else:
+            print(f"[Cryptor] WARNING: Can not reach \"{url}\"! Status code: {response.status_code}")
+
+        message = message[:-1].replace("|", ", ")
+        return (isSecure, message)
 
 
 
 ### TODO DEVELOPMENT AREA TO BE REMOVED ###
 if __name__ == "__main__":
-	print("--- Dev test of cryptor ---")
+    print("--- Dev test of cryptor ---")
 
-	cryptor = Cryptor()
+    cryptor = Cryptor()
 
-	password = "Test123"
-	print("Password: " + password)
+    PASSWORD = "Test123"
+    print("Password: " + PASSWORD)
 
-	hashedPassword = cryptor.hashPassword(password, True)
-	print("Hashed password: " + hashedPassword)
+    HASHED_PASSWORD = cryptor.hashKey(PASSWORD, True)
+    print("Hashed password: " + HASHED_PASSWORD)
 
-	otherPassword = "Test123a"
-	print("Other password: " + otherPassword)
+    OTHER_PASSWORD = "Test123a"
+    print("Other password: " + OTHER_PASSWORD)
 
-	isCorrect = cryptor.isCorrectPassword(otherPassword, hashedPassword)
-	print("Is other password = hash? " + str(isCorrect))
+    IS_CORRECT = cryptor.isCorrectKey(OTHER_PASSWORD, HASHED_PASSWORD)
+    print("Is other password = hash? " + str(IS_CORRECT))
 
-	text = "Hallo, ich bins!"
-	print("Text: " + text)
+    TEXT = "Hallo, ich bins!"
+    print("Text: " + TEXT)
 
-	encryptedText = cryptor.encryptText(text)
-	print("Encrypted text: " + encryptedText)
+    ENCRYPTED_TEXT = cryptor.encryptText(TEXT)
+    print("Encrypted text: " + ENCRYPTED_TEXT)
 
-	decryptedText = cryptor.decryptText(encryptedText)
-	print("Decrypted text: " + decryptedText)
+    DECRYPTED_TEXT = cryptor.decryptText(ENCRYPTED_TEXT)
+    print("Decrypted text: " + DECRYPTED_TEXT)
 
-	newPassword = cryptor.genPassword(4, True, True, True, True, "Test")
-	print("New password: " +  newPassword)
+    NEW_PASSWORD = cryptor.genPassword(4, True, True, True, True, "Test")
+    print("New password: " +  NEW_PASSWORD)
 
-	isSafe = cryptor.isSafe("123")
-	print("Is new password safe? " + str(isSafe[0]))
-	print("\tReason(s): " + str(isSafe[1]))
+    IS_SAFE = cryptor.isSafe(NEW_PASSWORD)
+    print("Is new password safe? " + str(IS_SAFE[0]))
+    print("\tReason(s): " + str(IS_SAFE[1]))
 
-	print("---------------------------")
+    print("---------------------------")
 ###########################################
-
